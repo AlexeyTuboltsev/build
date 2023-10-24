0d60e80844d242d93e4e4f50ef33afaed1411ac2
lexey <tblz@palavara.com>
2023-10-24 19:22:27 +0200

    add "rk3566-radxa-cm3-io" board config

--- board_configs.sh
+++ board_configs.sh
@@ -244,6 +244,14 @@ case ${BOARD} in
 		export CROSS_COMPILE=aarch64-linux-gnu-
 		CHIP="rk3566"
 		;;
+	"rk3566-radxa-cm3-io")
+		DEFCONFIG=rockchip_linux_defconfig
+		UBOOT_DEFCONFIG=radxa-cm3-io-rk3566_defconfig
+		DTB=rk3566-radxa-cm3-io.dtb
+		export ARCH=arm64
+		export CROSS_COMPILE=aarch64-linux-gnu-
+		CHIP="rk3566"
+		;;
 	"rk3566-rock-3c")
 		DEFCONFIG=rockchip_linux_defconfig
 		UBOOT_DEFCONFIG=rock-3c-rk3566_defconfig
